e660de3d845dc34f9a883485fb31d2cecaa3446b
EyeXion <elies.tali18@gmail.com>
2020-08-03 18:04:46 +0200

    Added character (not moving)

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -1,7 +1,11 @@
 import "phaser";
+import { Game } from "phaser";
 
 export class GameScene extends Phaser.Scene {
     ground : Phaser.Physics.Arcade.Image;
+    info : Phaser.GameObjects.Text;
+    score : number;
+    ppa : Phaser.Physics.Arcade.Sprite;
 constructor() {
     super({
       key: "GameScene"
@@ -9,14 +13,19 @@ constructor() {
   }
 
 init(params): void {
-    // TODO
+    this.score = 0;
   }
+
 preload(): void {
     this.load.image('ground','../assets/ground.png');
+    this.load.image('ppa','../assets/walk1.png');
   }
   
 create(): void {
     this.physics.add.staticImage(400,550,'ground');
+    this.physics.add.sprite(200,475,'ppa');
+    this.info = this.add.text(10, 10, 'Course du PPA ! Score : ' + this.score.toString(),
+      { font: '24px Arial Bold', fill: '#FBFBAC' });
 }
 
 update(time): void {
